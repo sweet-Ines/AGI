602178dfbdc1d28d498365f5f46897a418f5f599
Thalla <Temerita16@gmail.com>
2016-10-08 11:57:07 +0200

    Abfrage Größe und Spielfeld Initialisierung
    Fehlerbehandlung
    Darstellung Spielfeld

--- src/Spielfeld.java
+++ src/Spielfeld.java
@@ -1,12 +1,54 @@
+import java.util.Random;
 
 public class Spielfeld {
 
 	boolean[][] matrix;
 	boolean[][] matrixAlt;
+	int zufallszahl, breite, laenge;
 
 	public Spielfeld(int breite, int laenge){
+		this.breite=breite;
+		this.laenge=laenge;
 		matrix = new boolean [breite][laenge];
 		matrixAlt = new boolean [breite][laenge];
-
+		//while(true){
+			System.out.println("Matrix:" +breite*laenge);
+			matrixBefuellen();
+			//zufallszahl();
+		//}
+		
 	}
+	
+	public void matrixBefuellen(){
+		for(int b = 0; b<breite; b++){
+			for(int l = 0; l<laenge; l++){
+				double random = Math.random();
+				if(random >= 0.5){
+					matrix[b][l] = true;
+				}else{
+					matrix[b][l] = false;
+				}
+			}
+		}
+		matrixAlt = matrix;
+		ausgabe();
+	}
+	
+	public void ausgabe(){
+		for(int b = 0; b<breite; b++){
+			for(int l = 0; l<laenge; l++){
+				if(matrix[b][l] == true)
+				System.out.print("| X ");
+				else
+					System.out.print("|   ");
+			}
+			System.out.println();
+		}
+	}
+	
+//	public void zufallszahl(){
+//		Random random = new Random();
+//		zufallszahl = random.nextInt(breite*laenge);
+//		System.out.println(zufallszahl);
+//	}
 }
--- src/main.java
+++ src/main.java
@@ -1,22 +1,55 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Main {
 
+	static int breite;
+	static int laenge;
+	static boolean eingabe= false;
+
 	
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
 
-		int breite, laenge;
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Geben Sie die Breite der Matrix an:");
-		breite=scan.nextInt();
+		while(eingabe==false){
+		try{
+			Scanner scan = new Scanner(System.in);
+			System.out.println("Geben Sie die Breite der Matrix an:");
+			breite=scan.nextInt();
+			eingabe=true;
+		}
+		catch( Exception ex){
+			System.out.println("Es duerfen nur Zahlen angegeben werden.");
+			
+		}
+		}
 		
-		System.out.println("Geben Sie die Laenge der Matrix an:");
-		laenge=scan.nextInt();
+		eingabe=false;
 		
+		while(eingabe==false){
+			try{
+				Scanner scan = new Scanner(System.in);
+				System.out.println("Geben Sie die Laenge der Matrix an:");
+				laenge=scan.nextInt();
+				eingabe=true;
+			}
+			catch( Exception ex){
+				System.out.println("Es duerfen nur Zahlen angegeben werden.");
+				
+			}
+			}
+		System.out.println("Groesse der Matrix erstellt.");
+		Spielfeld s = new Spielfeld(breite, laenge);
 	}
+	
+	
+	
+//	public static void abfrage(){
+//		Scanner scan = new Scanner(System.in);
+//		System.out.println("Geben Sie die Breite der Matrix an:");
+//		breite=scan.nextInt();
+//		
+//		System.out.println("Geben Sie die Laenge der Matrix an:");
+//		laenge=scan.nextInt();
+//	}
 
 }
