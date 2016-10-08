bb240358371de7bbb284572a652cb770db6894cb
Thalla <Temerita16@gmail.com>
2016-10-08 11:04:57 +0200

    Matrix angelegt

--- src/Spielfeld.java
+++ src/Spielfeld.java
@@ -1,4 +1,12 @@
 
 public class Spielfeld {
 
+	boolean[][] matrix;
+	boolean[][] matrixAlt;
+
+	public Spielfeld(int breite, int laenge){
+		matrix = new boolean [breite][laenge];
+		matrixAlt = new boolean [breite][laenge];
+
+	}
 }
--- src/main.java
+++ src/main.java
@@ -1,9 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
+	
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 
+		int breite, laenge;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Geben Sie die Breite der Matrix an:");
+		breite=scan.nextInt();
+		
+		System.out.println("Geben Sie die Laenge der Matrix an:");
+		laenge=scan.nextInt();
+		
 	}
 
 }
