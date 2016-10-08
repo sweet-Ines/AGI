db3ba2a1c3e3288c1c36804fb18c18bb4a5cf162
SuzyCanon <djomosuzy@yahoo.fr>
2016-10-08 12:20:02 +0200

    changed

--- src/Spielfeld.java
+++ src/Spielfeld.java
@@ -3,10 +3,13 @@ public class Spielfeld {
 
 	boolean[][] matrix;
 	boolean[][] matrixAlt;
+	int [][] stelle;
 
 	public Spielfeld(int breite, int laenge){
 		matrix = new boolean [breite][laenge];
 		matrixAlt = new boolean [breite][laenge];
 
 	}
+	 
+	
 }
--- src/main.java
+++ src/main.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Scanner;
 
-public class Main {
+public class main {
 
 	
 	public static void main(String[] args) throws IOException {
